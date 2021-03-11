d47c314d110bc40baf5987dae887bc4ed7a05287
heba-ali2030 <heba.galaa@gmail.com>
2021-03-11 22:52:51 +0200

    add

--- student.py
+++ student.py
@@ -1,9 +1,16 @@
-list=['a','b','c','d']
+students= [["Ahmed ali",{"class": "first", "grade": "excellent"}],
+["Mona Salah",{"class": "second", "grade": "excellent"}],
+["Mai Ahmed",{"class": "third", "grade": "very good"}],
+["Heba Ali",{"class": "second", "grade": "good"}],
+["Hossam Hassan",{"class": "first", "grade": "good"}]]
+              
+def report1(student_data):
+    """ prints student report """
+    print("student              data")
+    for student_item in student_data:
+        print(student_item[0], "        ", student_item[1])
 
-students= {'name': list,
-'semester':'first', 
-'degree':'excellent'}
-for student in students:
-    print(students)
+
+report1(students)
 
     
